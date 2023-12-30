4855e22f1fee17622306fcbcac6a8b0373bed9fe
Gustavo Silva <devgustavosilva@gmail.com>
2023-12-30 13:21:54 -0300

    Fix next fast reload

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, _) => ({
+        ...config,
+        watchOptions: {
+          ...config.watchOptions,
+          poll: 400,
+          aggregateTimeout: 300,
+        },
+    }),
+}
 
 module.exports = nextConfig
