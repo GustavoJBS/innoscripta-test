7097fb62459ba31b5b1d97b949454bc6bf45f1e9
Gustavo Silva <devgustavosilva@gmail.com>
2023-12-30 14:34:57 -0300

    Change Location News Service

--- backend/app/Services/News/Article.php
+++ backend/app/Services/News/Article.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\News;
 
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Support\Facades\Http;
 
-class News
+class Article
 {
     private PendingRequest $api;
 
@@ -15,5 +15,13 @@ class News
             ->withToken(config('services.news-api.key'))	
             ->acceptJson()
             ->asJson();
+    }
+
+    public function get(): mixed
+    {
+        return $this->api
+            ->get('everything', [
+                'sources' => 'globo',
+            ])->json();
     }
 }
