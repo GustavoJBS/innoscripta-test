17b8687e79a763c7acf3d6d4fb4c612289373cfd
Gustavo Silva <devgustavosilva@gmail.com>
2023-12-31 18:28:24 -0300

    Create  endpoint to Save Preferences

---
+++ backend/app/Http/Controllers/PreferenceController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+class PreferenceController extends Controller
+{
+    public function save(): Response|JsonResponse
+    {
+        $validatedData = $this->validate(request(), [
+            'languages' => ['array', 'min:1'],
+            'sources' => ['array', 'min:3'],
+            'sources.*' => ['numeric', 'exists:sources,id'],
+            'categories' => ['array', 'min:4'],
+            'categories.*' => ['numeric', 'exists:categories,id'],
+        ]);
+
+        request()->user()
+            ->preference()
+            ->update([
+                'languages' => request('languages', []),
+                'sources' => request('sources', []),
+                'categories' => request('categories', []),
+            ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User Preferences saved Successfully',
+            'user' => auth()->user()->load('preference'),
+        ], Response::HTTP_CREATED);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\PreferenceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('preference', [PreferenceController::class, 'save']);
 });
