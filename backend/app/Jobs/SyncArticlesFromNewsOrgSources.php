e361671b9a8c0db01c1231a5be35aec0a8c813f7
Gustavo Silva <devgustavosilva@gmail.com>
2023-12-31 12:22:10 -0300

    Add Command to save Sources and Articles

---
+++ backend/app/Jobs/SyncArticlesFromNewsOrgSources.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Article as ArticleModel;
+use App\Models\Category;
+use App\Models\Source;
+use App\Services\News\NewsOrg\Article;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\RateLimited;
+use Illuminate\Queue\SerializesModels;
+
+class SyncArticlesFromNewsOrgSources implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(
+        public Source $source,
+        public Category $category
+    ) {
+    }
+
+    public function handle(): void
+    {
+        $articlesService = new Article(...config('services.news-api'));
+
+        $articlesService
+            ->get($this->source->name)
+            ->each(fn (array $article) => $this->saveArticle([
+                'title' => $article['title'],
+                'url' => $article['url'],
+                'image' => optional($article)['urlToImage'],
+                'content' => $article['content'],
+                'description' => $article['description'],
+                'published_at' => Carbon::parse($article['publishedAt']),
+                'language' => $this->source->language,
+                'category_id' => $this->category->id,
+                'source_id' => $this->source->id,
+            ]));
+    }
+
+    private function saveArticle(array $article): ArticleModel
+    {
+        return ArticleModel::query()
+            ->updateOrCreate(
+                ['title' => $article['title']],
+                [...collect($article)->except('title')]
+            );
+    }
+}
