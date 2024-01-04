094f22dc9f25c51162d2b33211ad79a75562fa1c
Gustavo Silva <devgustavosilva@gmail.com>
2024-01-04 05:49:06 -0300

    Change pagination component

--- frontend/src/app/(admin-routes)/home/page.tsx
+++ frontend/src/app/(admin-routes)/home/page.tsx
@@ -2,7 +2,7 @@
 
 import Article from "@/components/Article";
 import Preference, { CheckboxArray, PreferenceInterface } from "@/components/Preference";
-import { Divider, Spinner } from "@nextui-org/react";
+import { Divider, Pagination, Spinner } from "@nextui-org/react";
 import axios from "axios";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
@@ -148,7 +148,11 @@ export default function Home() {
 
             {
                 loading
-                    ? (<Spinner />) 
+                    ? (
+                        <div className="w-full flex justify-center">
+                            <Spinner />
+                        </div>
+                    ) 
                     : (
                         <div className="flex flex-col">
                             <div className="w-full grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
@@ -177,32 +181,16 @@ export default function Home() {
                     )
             }
 
-
             <div className="flex justify-center my-6">
-                <button
-                    onClick={() => setPage(page - 1)}
-                    className="cursor-pointer hover:opacity-60 duration-300"
-                    disabled={page === 1}
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                </button>
-
-                <span className="font-bold text-lg mx-4 my-auto">
-                    {page}
-                </span>
-
-                <button
-                    onClick={() => setPage(page + 1)}
-                    className="cursor-pointer hover:opacity-60 duration-300"
-                    disabled={page === lastPage}
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                </button>
+                <Pagination 
+                    showShadow 
+                    showControls 
+                    total={lastPage} 
+                    initialPage={1} 
+                    onChange={setPage} 
+                />
             </div>
+
         </div>
     )
 }
